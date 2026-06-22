2dfb60ba97b0e17b27cf44f931387c53d6dd5e60
LOKI25557 <lokeshgowdads88@gmail.com>
2026-06-22 19:18:20 +0530

    Feature: Add initial application screens

---
+++ mobile-app/src/screens/CameraScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const CameraScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>CameraScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+});
+
+export default CameraScreen;

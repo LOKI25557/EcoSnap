bc2cda5c0d4735877138e9fe3c1de2a7d6e4249f
LOKI25557 <lokeshgowdads88@gmail.com>
2026-08-13 13:18:47 +0530

    test(jest): configure AsyncStorage native module mocking

---
+++ mobile-app/jest.setup.js
@@ -0,0 +1,3 @@
+// Mock AsyncStorage
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
